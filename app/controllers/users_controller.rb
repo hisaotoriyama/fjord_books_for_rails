b07c9b7b4e3b94dabb21d93c86f4822e2ede1032
hisaotoriyama <hisao.toriyama@gmail.com>
2020-10-19 23:39:16 +0900

    creation of users list and user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def show; end
+  def show
+    # @user = current_user
+    @user = User.find(params[:id])
+    p "****"
+    p @user.id
+    p @user.name
+  end
+
+  def index
+    @users = User.all
+  end
 end
