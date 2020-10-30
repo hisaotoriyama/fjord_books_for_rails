1d4119bb0f1d0b84ab118b1c4293b29a807660ff
hisaotoriyama <hisao.toriyama@gmail.com>
2020-10-30 16:49:00 +0900

    mislaneous adjustment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  USERBOOKS_NUMBER = 8
+
   def show
     @user = User.find(params[:id])
+    @usersbooks = @user.books.page(params[:page]).per(USERBOOKS_NUMBER)
   end
 
   def index
