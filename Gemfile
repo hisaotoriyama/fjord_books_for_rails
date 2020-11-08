ff0d4307a0ba2a3511e7e62e060a205d80bffa2a
hisaotoriyama <hisao.toriyama@gmail.com>
2020-11-09 08:27:48 +0900

    modify seeds.rb by Faker

--- Gemfile
+++ Gemfile
@@ -70,3 +70,4 @@ gem "devise"
 gem "omniauth"
 gem "omniauth-github"
 gem "dotenv-rails"
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,73 +1,13 @@
 # frozen_string_literal: true
 
-20.times do |n|
+USER_ID_RANGE = (18..25)
+USER_ID_RANGE.each do |num|
+  5.times do |n|
     Book.create!(
-      title: "test book No.#{n + 1}",
-      memo: "contents#{n + 1}",
-      author: "test author No.#{n + 1}",
-      user_id: 18
+      title: Faker::Book.title,
+      memo: Faker::Book.genre,
+      author: Faker::Book.author,
+      user_id: num
     )
   end
-
-20.times do |n|
-  Book.create!(
-    title: "test book No.#{n + 1}",
-    memo: "contents#{n + 1}",
-    author: "test author No.#{n + 1}",
-    user_id: 19
-  )
-end
-
-20.times do |n|
-  Book.create!(
-    title: "test book No.#{n + 1}",
-    memo: "contents#{n + 1}",
-    author: "test author No.#{n + 1}",
-    user_id: 20
-  )
-end
-
-20.times do |n|
-  Book.create!(
-    title: "test book No.#{n + 1}",
-    memo: "contents#{n + 1}",
-    author: "test author No.#{n + 1}",
-    user_id: 21
-  )
-end
-
-20.times do |n|
-  Book.create!(
-    title: "test book No.#{n + 1}",
-    memo: "contents#{n + 1}",
-    author: "test author No.#{n + 1}",
-    user_id: 22
-  )
-end
-
-20.times do |n|
-  Book.create!(
-    title: "test book No.#{n + 1}",
-    memo: "contents#{n + 1}",
-    author: "test author No.#{n + 1}",
-    user_id: 23
-  )
-end
-
-20.times do |n|
-  Book.create!(
-    title: "test book No.#{n + 1}",
-    memo: "contents#{n + 1}",
-    author: "test author No.#{n + 1}",
-    user_id: 24
-  )
-end
-
-20.times do |n|
-  Book.create!(
-    title: "test book No.#{n + 1}",
-    memo: "contents#{n + 1}",
-    author: "test author No.#{n + 1}",
-    user_id: 25
-  )
 end
