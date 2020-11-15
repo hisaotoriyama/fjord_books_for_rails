84f8508925e7316c75487f0625aa161b8707cad0
hisaotoriyama <hisao.toriyama@gmail.com>
2020-11-15 09:26:36 +0900

    add InverseOfOption and eliminate option in RegistController

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable
   has_many :books, dependent: :destroy
-  has_many :active_relationships, class_name: 'Relationship', foreign_key: :following_id, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: :following_id, dependent: :destroy, inverse_of: 'following'
   has_many :followings, through: :active_relationships, source: :follower
-  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy, inverse_of: 'follower'
   has_many :followers, through: :passive_relationships, source: :following
   has_one_attached :avatar
   attr_accessor :current_password
