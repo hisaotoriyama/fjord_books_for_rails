42c6086f0b3ea43db799ae07fb4730163dc3e5da
hisaotoriyama <hisao.toriyama@gmail.com>
2020-11-22 09:39:58 +0900

    minor adjstment including alert in i18n

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,27 +4,18 @@ class ReportsController < ApplicationController
   before_action :set_report, only: %i[show edit update destroy]
   REPORTS_PER_PAGE_SIZE = 5
 
-  # GET /reports
-  # GET /reports.json
   def index
     @reports = Report.order(:created_at, :id).page(params[:page]).per(REPORTS_PER_PAGE_SIZE)
-    # @reports = current_user.reports.order(:created_at, :id).page(params[:page]).per(REPORTS_PER_PAGE_SIZE)
   end
 
-  # GET /reports/1
-  # GET /reports/1.json
   def show; end
 
-  # GET /reports/new
   def new
     @report = Report.new
   end
 
-  # GET /reports/1/edit
   def edit; end
 
-  # POST /reports
-  # POST /reports.json
   def create
     @report = Report.new(report_params)
 
@@ -39,8 +30,6 @@ class ReportsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reports/1
-  # PATCH/PUT /reports/1.json
   def update
     respond_to do |format|
       if @report.update(report_params)
@@ -53,8 +42,6 @@ class ReportsController < ApplicationController
     end
   end
 
-  # DELETE /reports/1
-  # DELETE /reports/1.json
   def destroy
     @report.destroy
     respond_to do |format|
@@ -65,12 +52,10 @@ class ReportsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_report
     @report = Report.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def report_params
     params.require(:report).permit(:title, :body, :user_id)
   end
