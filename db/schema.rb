6fbbc61f4ba512dc29bef94e4400c0dcdf6f60e2
hisaotoriyama <hisao.toriyama@gmail.com>
2020-12-02 19:38:33 +0900

    add index and foreign_key in reports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_234500) do
+ActiveRecord::Schema.define(version: 2020_12_02_095837) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_234500) do
     t.integer "user_id", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -92,4 +93,5 @@ ActiveRecord::Schema.define(version: 2020_11_30_234500) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "books", "users"
   add_foreign_key "comments", "users"
+  add_foreign_key "reports", "users"
 end
